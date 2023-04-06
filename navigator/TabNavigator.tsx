19f293eab8752f05ede1651a65043383af267798
Michał Bryk <galeriakoloru90210@gmail.com>
2023-04-06 21:30:44 +0200

    complete ui for bottom tab naviator

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { CustomersScreen, OrdersScreen } from "../screens";
-import { useNavigation } from "@react-navigation/native";
+import { Icon } from "@rneui/themed";
 
 export type TabStackParamList = {
   Customers: undefined;
@@ -12,7 +12,19 @@ const Tab = createBottomTabNavigator<TabStackParamList>();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Customers") {
+            return <Icon name="users" type="entypo" color={focused ? "#59C1CC" : "gray"} />;
+          } else if (route.name === "Orders") {
+            return <Icon name="box" type="entypo" color={focused ? "#EB6A7C" : "gray"} />;
+          }
+        },
+      })}
+    >
       <Tab.Screen name="Customers" component={CustomersScreen} options={{ headerShown: false }} />
       <Tab.Screen name="Orders" component={OrdersScreen} options={{ headerShown: false }} />
     </Tab.Navigator>
