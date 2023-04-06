1f170bf452431f5f6c18eac6b7d7bee75aedc4f2
Michał Bryk <galeriakoloru90210@gmail.com>
2023-04-06 23:23:28 +0200

    setup stepzen and firebase

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -8,7 +8,7 @@ const CustomersScreen = () => {
       <Image source={{ uri: "https://links.papareact.com/3jc" }} className="w-full h-64" />
       <TextInput
         placeholder="Search by Customer"
-        value={input}
+        value={input.toLowerCase()}
         onChangeText={setInput}
         className="bg-white p-4 px-10 flex items-center justify-center flex-1 text-xl rounded-xl mx-2"
       />
