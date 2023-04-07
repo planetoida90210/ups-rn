5806a9d9a966a8bcedeb93987c74b1933fa6c8b7
Michał Bryk <galeriakoloru90210@gmail.com>
2023-04-07 16:41:13 +0200

    seting up apollo client

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 //external imports
 import { CustomersScreen } from "./screens";
 import RootNavigator from "./navigator/RootNavigator";
 
+const client = new ApolloClient({
+  uri: "https://saojose.stepzen.net/api/hipster-fly",
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
